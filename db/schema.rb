157b9c3eb8178679c0a692f27586b2f8177750c0
karei0106 <kosei445.874@gmail.com>
2021-01-30 21:26:48 +0900

    商品出品機能修正５

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_111633) do
+ActiveRecord::Schema.define(version: 2021_01_23_152448) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,22 +31,6 @@ ActiveRecord::Schema.define(version: 2021_01_24_111633) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "explanation", null: false
-    t.integer "category_id", null: false
-    t.integer "state_id", null: false
-    t.integer "delivery_fee_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "day_id", null: false
-    t.integer "price", null: false
-    t.bigint "user_id"
-    t.integer "genre_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -83,6 +67,5 @@ ActiveRecord::Schema.define(version: 2021_01_24_111633) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "articles", "users"
   add_foreign_key "items", "users"
 end
