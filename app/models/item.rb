4ffadc40f0d7f54694e33820ca6fcd5fd628ec11
karei0106 <kosei445.874@gmail.com>
2021-02-01 20:16:15 +0900

    商品一覧表示機能修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,4 +21,11 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :day_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :day
+  belongs_to :delivery_fee
+  belongs_to :prefecture
+  belongs_to :state
 end
